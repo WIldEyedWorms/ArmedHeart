84007dde03093f2c7fe31b0266425e0e7cdd32b5
WildEyedWorms <toshihikoaoyanagi@gmail.com>
2015-05-05 02:08:52 +0900

    オートビルド設定

---
+++ ArmedHeart/Assets/Editor/BuildBatch.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class BuildBatch {
+	private static void BuildAndroid()
+	{
+		EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.Android );
+		string[] allScene = GetScenes ();
+		PlayerSettings.bundleIdentifier = "jp.wildeyedworms.armedheart";
+		PlayerSettings.statusBarHidden = true;
+		BuildPipeline.BuildPlayer( allScene,
+		                          "hoge.apk",
+		                          BuildTarget.Android,
+		                          BuildOptions.None
+		                          );
+	}
+	
+	private static string[] GetScenes() {
+		ArrayList levels = new ArrayList();
+		foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+			if (scene.enabled) {
+				levels.Add(scene.path);
+			}
+		}
+		return (string[]) levels.ToArray(typeof(string));
+	}
+}
