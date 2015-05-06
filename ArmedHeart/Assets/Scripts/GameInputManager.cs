3981f82df0bcb63f6ae069834e1bfc848128151a
WildEyedWorms <toshihikoaoyanagi@gmail.com>
2015-05-06 11:26:19 +0900

    add button

---
+++ ArmedHeart/Assets/Scripts/GameInputManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum eGameButtonType
+{
+	LeftUp,
+	LeftDown,
+	RightUp,
+	RightDown
+};
+
+public class GameInputManager : MonoBehaviour {
+
+	//************ Button Up ******************// 
+	public void OnLeftDownButtonUp()
+	{
+	}
+
+	public void OnRightDownButtonUp()
+	{
+	}
+
+	public void OnLeftUpButtonUp()
+	{
+	}
+
+	public void OnRightUpButtonUp()
+	{
+	}
+	//************* Button Down ****************//
+	public void OnLeftDownButtonDown()
+	{
+	}
+	
+	public void OnRightDownButtonDown()
+	{
+	}
+	
+	public void OnLeftUpButtonDown()
+	{
+	}
+	
+	public void OnRightUpButtonDown()
+	{
+	}
+}
