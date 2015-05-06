3b7c2b23cbfe8f58dd53a07bd795a85f6c355e8e
WildEyedWorms <toshihikoaoyanagi@gmail.com>
2015-05-06 21:57:28 +0900

    add Gyaro

---
+++ ArmedHeart/Assets/Scripts/Common/Component/GyroScopeComponent.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GyroScopeComponent : MonoBehaviour {
+
+	#if UNITY_EDITOR
+	Vector2 mPrevMousePos;
+	#endif
+	// Update is called once per frame
+	void Update () {
+		
+		#if UNITY_EDITOR
+		Vector2 tCurrentMousePos = Input.mousePosition;
+		Vector2 tDeltaMousePos = tCurrentMousePos - mPrevMousePos;
+		
+		tDeltaMousePos.Normalize();
+		
+		tDeltaMousePos *= 0.02f;
+		Quaternion tCurrentRotation = transform.localRotation;
+		transform.localRotation = new Quaternion(tCurrentRotation.x + tDeltaMousePos.y, tCurrentRotation.y - tDeltaMousePos.x, tCurrentRotation.z, tCurrentRotation.w );
+		
+		mPrevMousePos = tCurrentMousePos;
+		#else
+		Quaternion tRotRH = Input.gyro.attitude;
+		Quaternion tRot = new Quaternion(-tRotRH.x, -tRotRH.z, -tRotRH.y, tRotRH.w) * Quaternion.Euler(90f, 0f, 0f);
+		
+		transform.localRotation = tRot;
+		
+		#endif
+	}
+}
