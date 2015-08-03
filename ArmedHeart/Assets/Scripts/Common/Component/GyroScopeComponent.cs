edb6b004d41c623989369a4b835ef46b7670251f
WildEyedWorms <toshihikoaoyanagi@gmail.com>
2015-08-04 08:58:52 +0900

    カメラ移動修正

--- ArmedHeart/Assets/Scripts/Common/Component/GyroScopeComponent.cs
+++ ArmedHeart/Assets/Scripts/Common/Component/GyroScopeComponent.cs
@@ -5,20 +5,44 @@ public class GyroScopeComponent : MonoBehaviour {
 
 	#if UNITY_EDITOR
 	Vector2 mPrevMousePos;
+	Vector3 mEulerAngles;
 	#endif
 	// Update is called once per frame
 	void Update () {
 		
 		#if UNITY_EDITOR
 		Vector2 tCurrentMousePos = Input.mousePosition;
-		Vector2 tDeltaMousePos = tCurrentMousePos - mPrevMousePos;
-		
-		tDeltaMousePos.Normalize();
-		
-		tDeltaMousePos *= 0.02f;
-		Quaternion tCurrentRotation = transform.localRotation;
-		transform.localRotation = new Quaternion(tCurrentRotation.x + tDeltaMousePos.y, tCurrentRotation.y - tDeltaMousePos.x, tCurrentRotation.z, tCurrentRotation.w );
-		
+		if( Input.GetMouseButton(0) )
+		{
+			Vector2 tDeltaMousePos = tCurrentMousePos - mPrevMousePos;
+			
+			tDeltaMousePos.Normalize();
+			
+			tDeltaMousePos *= 0.5f;
+			Quaternion tCurrentRotation = transform.localRotation;
+			mEulerAngles = transform.eulerAngles;
+			mEulerAngles.y += tDeltaMousePos.x;
+			if( mEulerAngles.y > 360.0f )
+			{
+				mEulerAngles.y -=360.0f;
+			}
+			else if( mEulerAngles.y < 0.0f )
+			{
+				mEulerAngles.y += 360.0f;
+			}
+
+			mEulerAngles.x -= tDeltaMousePos.y;
+			if( mEulerAngles.x > 360.0f )
+			{
+				mEulerAngles.x -= 360.0f;
+			}
+			else if( mEulerAngles.x < 0.0f )
+			{
+				mEulerAngles.x += 360.0f;
+			}
+
+			transform.eulerAngles = mEulerAngles;
+		}
 		mPrevMousePos = tCurrentMousePos;
 		#else
 		Quaternion tRotRH = Input.gyro.attitude;
